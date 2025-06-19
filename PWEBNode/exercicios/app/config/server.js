cf50a913600fba5404d1493443ee74263dbe43cb
Murilo Bertoni Ismirim <143854509+muriloismirim@users.noreply.github.com>
2025-06-18 21:47:04 -0300

    Delete PWEBNode/exercicios directory

--- PWEBNode/exercicios/app/config/server.js
+++
@@ -1,7 +0,0 @@
-let express = require('express')
-
-let app=express();
-
-app.set('view engine', 'ejs');
-app.set('views','./app/views');
-module.exports=app;
--- PWEBNode/exercicios/app/routes/professores.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app){
-    app.get('/informacao/professores', function(req,res){
-        res.render('informacao/professores')
-    })
-}
